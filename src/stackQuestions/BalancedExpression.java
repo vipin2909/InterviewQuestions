0c1dcd4515fb2f6441426bf49f90b9a358bba985
vipin2909 <vipinsharma2909@gmail.com>
2022-09-07 16:59:32 +0530

    program to check if expressions are balanced or not

--- src/stackQuestions/BalancedExpression.java
+++ src/stackQuestions/BalancedExpression.java
@@ -1,2 +1,58 @@
-package stackQuestions;public class BalancedExpression {
+package stackQuestions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class BalancedExpression {
+
+    public boolean checkIfExpressionIsBalanced(String expression) {
+        Stack<Character> stack = new Stack<Character>()
+                ;
+        for(int index = 0; index < expression.length(); index++) {
+            char ch = expression.charAt(index);
+            if(ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
+            }
+            if(ch == ']') {
+                if(!stack.isEmpty() && stack.peek() != '[') {
+                    return false;
+                }
+                else {
+                    stack.pop();
+                }
+            }
+            if(ch == ')') {
+                if(!stack.isEmpty() && stack.peek() != '(') {
+                    return false;
+                }
+                else {
+                    stack.pop();
+                }
+            }
+            if(ch == '}') {
+                if(!stack.isEmpty() && stack.peek() != '{') {
+                    return false;
+                }
+                else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+
+    }
+    public static void main(String[] args) {
+        BalancedExpression balancedExpression = new BalancedExpression();
+        boolean ans  = balancedExpression.checkIfExpressionIsBalanced("{}{((()))[]([()({}{{}{([])}})][][][])}");
+        System.out.println("Expression is balanced ? " + ans);
+    }
+
+    @Test
+    public void test0() {
+        Assert.assertTrue(checkIfExpressionIsBalanced("{[({}{}{}[][()()({}{()([][{}{()()}])})][])]}{}{}"));
+    }
 }
